18756bbf0a7db84cd196375327832c800f2ef63f
”Danese“ <Jiacheng_325@outlook.com>
2024-11-09 23:23:15 +0800

    11.9shell study

---
+++ shell_study/shell_record.py
@@ -0,0 +1,30 @@
+"""
+shell
+
+cd  ---Switch directory
+    "." ---current directory
+    ".." ---parent directory 上级
+ls ---what lives in a given directory
+mv ---rename/move a file
+cp ---copy a file
+mkdir ---make a new directory
+cat ---concat file or show file content #missing:~$ echo hello > hello.txt
+                    missing:~$ cat hello.txt
+                    hello
+                    missing:~$ cat < hello.txt
+                    hello
+                    missing:~$ cat < hello.txt > hello2.txt
+                    missing:~$ cat hello2.txt
+                    hello
+"｜"组合命令
+"<" file ---output
+">" file ---input
+some flags:("-"+"some letter")：-h means -help
+programs:"man"(use"q" to exit)---check values or how to use
+"""
+"""
+BASH
+
+
+
+"""
